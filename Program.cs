f16fe7c2f0d9ef88e6d8d15cad1de479bbc02ce3
DavidHamelin <hamelindavid@hotmail.fr>
2018-12-17 17:04:20 +0100

    exercice non corrigé

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercice_08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quel est le rayon r ?");
+            double r = double.Parse(Console.ReadLine());
+
+            double pi = Math.PI;
+            double perimetre = 2 * pi * r;
+            double surface = Math.Pow(r, 2) * pi;
+
+            Console.WriteLine("Le périmetre est " + perimetre);
+            Console.WriteLine("La surface est " + surface);
+
+        }
+    }
+}
